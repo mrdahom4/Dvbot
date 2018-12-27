fc3b7a9f31af0f135f126660b778a2f5cee30425
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-27 19:53:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const fs = require("fs"); 
 const Canvas = require("canvas");
 const jimp = require("jimp");
 let points = {}
