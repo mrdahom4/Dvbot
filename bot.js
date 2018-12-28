b584a166d92bab1aaf707dd51832903a7ed8af3d
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-28 11:24:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,6 +177,39 @@ client.on('message', function(message) {
 
 	
 
+client.on('message', message => {
+if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'move')) {
+ if (message.member.hasPermission("MOVE_MEMBERS")) {
+ if (message.mentions.users.size === 0) {
+ return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : " +prefix+ "move [USER]``")
+}
+if (message.member.voiceChannel != null) {
+ if (message.mentions.members.first().voiceChannel != null) {
+ var authorchannel = message.member.voiceChannelID;
+ var usermentioned = message.mentions.members.first().id;
+var embed = new Discord.RichEmbed()
+ .setTitle("Succes!")
+ .setColor("#000000")
+ .setDescription(`لقد قمت بسحب <@${usermentioned}> الى الروم الصوتي الخاص بك✅ `)
+var embed = new Discord.RichEmbed()
+.setTitle(`You are Moved in ${message.guild.name}`)
+ .setColor("RANDOM")
+.setDescription(`**<@${message.author.id}> Moved You To His Channel!\nServer --> ${message.guild.name}**`)
+ message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
+message.guild.members.get(usermentioned).send(embed)
+} else {
+message.channel.send("``لا تستطيع سحب "+ message.mentions.members.first() +" `يجب ان يكون هذه العضو في روم صوتي`")
+}
+} else {
+ message.channel.send("**``يجب ان تكون في روم صوتي لكي تقوم بسحب العضو أليك``**")
+}
+} else {
+message.react("❌")
+}
+ }
+});
+
 	
 	client.on('message', msg => {
   if (msg.author.bot) return;
@@ -235,6 +268,8 @@ client.on('message', message => {
  ❖ #unmute <mention> ➾ فك الاسكات من العضو
   
  ❖ #bc <message> ➾ لارسال رسالة لجميع اعضاء السيرفر
+
+ ❖ #move ➾ لسحب العضو الي الروم الصوتي
 
  ❖ #voice <name> ➾ عمل روم فيوس اونلين
 ╔[❖════════════❖]╗
