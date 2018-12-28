e9387e5d0c72f0f8c0ca180f3a452ebc4f51a6cd
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-28 07:24:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ hours = 12;
 }
 
   var filter = m => m.author.id === message.author.id;
-  if(message.content.startsWith(prefix + "giveaway.")) {
+  if(message.content.startsWith(prefix + "gav")) {
 
     if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
     message.channel.send(`:eight_pointed_black_star:| **Send Name channel For the Giveaway**`).then(msg => {
